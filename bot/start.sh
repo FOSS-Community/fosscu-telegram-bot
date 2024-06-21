dc0434b87db88478c8b976455baf6b87a186620e
ksauraj <gitsauraj@gmail.com>
2024-06-21 19:54:40 +0530

    bot: fix /start

--- bot/start.sh
+++ bot/start.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 start() {
-    tg --replymarkdownv2msg "$RET_CHAT_ID" "$RET_MSG_ID" "This bot is managed by **FOSSCU-K** team\."
+    tg --replymarkdownv2msg "$RET_CHAT_ID" "$RET_MSG_ID" "*FOSSCU\-K Bot* is a Telegram bot written in Bash that interacts with GitHub to fetch and display organization members, open issues, and pull requests\.
+
+> _Additionally, it provides general\-purpose functionalities like fetching user information, downloading profile pictures, and some fun features like calculating IQ for fun\._
+
+To get started message _/help_
+
+
+[Join FOSSCU\-K](https://github.com/FOSS-Community)        [Bot Source](https://github.com/FOSS-Community/fosscu-telegram-bot)"
 }
